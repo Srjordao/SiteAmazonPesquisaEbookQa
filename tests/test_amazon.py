4749f81bb0080e6d04d4875df7571d9965264d70
Rogério Jordão <ro_jordao@hotmail.com>
2023-07-12 16:19:30 -0300

    Update test_amazon.py

--- tests/test_amazon.py
+++ tests/test_amazon.py
@@ -23,7 +23,7 @@ class AmazonTest:
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--disable-dev-shm-usage")
 
-        self.driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
+        self.driver = webdriver.Chrome(ChromeDriverManager().install())
         self.driver.get("https://www.amazon.com.br/")
         self.driver.maximize_window()
         self.elements = Elements(self.driver)
